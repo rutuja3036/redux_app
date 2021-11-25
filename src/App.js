0daf92a9c340ad6782537b7b48b653a1dcc9565a
rutuja <rutujaingale0230@example.com>
2021-11-25 11:31:52 +0530

    profile and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      hello World
+      <Profile/>
+      <Login/>
     </div>
   );
 }
