51c3dbf5908ea160d30f69a46340efcc7d47aba6
rutuja <rutujaingale0230@example.com>
2021-11-25 11:43:48 +0530

    creating redux strore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+ const store = configureStore({
+  reducer:{},
+})
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
